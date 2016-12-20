0683a075f3a13a37c0bb8e219995fce60d05e3de
srikanthlogic <srik.lak@gmail.com>
2016-12-20 05:36:53 +0530

    Add generic updateL10N method

--- js/l10n.js
+++ js/l10n.js
@@ -17,3 +17,16 @@ function updateQRCodeL10N() {
 		$('[data-i18n = upiqrc-btn-pay]' ).text($.i18n( 'upiqrc-btn-pay', $('#pn').val() ));
 	});
 }
+
+function updateL10N(jsonFile) {
+	'use strict';
+	var i18n = $.i18n();
+	var language = $('#lang').val();
+	i18n.locale = language;
+
+	i18n.load( 'i18n/' + jsonFile, language ).done( function(){
+		$("*[data-i18n]").each(function(){
+			$(this).text($.i18n($(this).data('i18n')))
+		});
+	});
+}
